8a0485347d5c38c60bb1ae7ea5852d2ef9126944
Jacqueline Silva <jacque.sx@hotmail.com>
2022-06-16 11:17:34 -0300

    :sparkles: feat: requisito 02, crie o endpoint GET "/talker/:id"

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_ERR_STATUS = 404;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -17,6 +18,16 @@ app.get('/talker', (_request, response) => {
   const dataTalker = JSON.parse(fs.readFileSync('talker.json', 'utf8'));
   const talker = dataTalker.length === 0 ? [] : dataTalker;
   response.status(HTTP_OK_STATUS).json(talker);
+});
+
+app.get('/talker/:id', (request, response) => {
+  const { id } = request.params;
+  const dataTalker = JSON.parse(fs.readFileSync('talker.json', 'utf8'));
+  const talkerID = dataTalker.find((talker) => talker.id === +id);
+  if (!talkerID) {
+    return response.status(HTTP_ERR_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  response.status(HTTP_OK_STATUS).json(talkerID);
 });
 
 app.listen(PORT, () => {
